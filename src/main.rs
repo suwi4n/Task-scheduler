2670af89eb3cf2058a7ce1a29ef2b7de252069e2
suwi4n <fortniteggdropnavi@gmail.com>
2026-07-14 19:03:21 +0700

    сделал планировщик вытесняющим и добавил удаление одноразовых задач из списка

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,119 @@
-use std::{sync::{Arc, Mutex}, time::Duration};
+use core::task;
+use std::{thread::current, time::Duration};
 use serde::{Serialize, Deserialize};
-use std::fs;
+use std::sync::Arc;
+use tokio::{fs, sync::Mutex};
 use chrono::{DateTime, Utc};
 
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 enum Trigger {
     Interval(u64),
+    Once,
     Cron(String),
     SystemEvent(String),
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct Task {
+pub struct Task {
     pub task_id: u32,
     pub command: String,
     pub args: Vec<String>,
     pub trigger: Trigger,
     #[serde(default)]
     pub next_run: Option<DateTime<Utc>>,
-    #[serde(skip)]
-    pub is_running: bool,
 }
 
-pub fn load_task(path: &str) -> Result<Vec<Task>, Box<dyn std::error::Error>> {
-    let tasks: Vec<Task> = serde_json::from_str(&fs::read_to_string(path)?)?;
+pub async fn load_task(path: &str) -> Result<Vec<Task>, Box<dyn std::error::Error>> {
+    let tasks: Vec<Task> = serde_json::from_str(&fs::read_to_string(path).await?)?;
     Ok(tasks)
 }
-pub fn save_task(path: &str, tasks: &Vec<Task>) -> Result<(), Box<dyn std::error::Error>> {
-    let writing = fs::write(path, serde_json::to_string_pretty(&tasks)?)?;
+pub async fn save_task(path: &str, tasks: &Vec<Task>) -> Result<(), Box<dyn std::error::Error>> {
+    let writing = fs::write(path, serde_json::to_string_pretty(tasks)?).await?;
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (tx, mut rx) = tokio::sync::mpsc::channel::<u32>(100);
 
     let path = "tasks.json";
 
-    let mut tasks = load_task(&path)?;
+    let mut init_tasks = load_task(path).await?;
     let now = Utc::now();
-    for task in &mut tasks {
-        if let Trigger::Interval(secs) = task.trigger {
-            task.next_run = Some(now + chrono::Duration::seconds(secs as i64));
+    for task in &mut init_tasks {
+        match task.trigger {
+            Trigger::Interval(secs) => {
+                task.next_run = Some(now + chrono::Duration::seconds(secs as i64));
+            }
+            Trigger::Once => {
+                if task.next_run.is_none() {
+                    task.next_run = Some(now);
+                }
+            }
+            _ => {}
         }
     }
+    let tasks = Arc::new(Mutex::new(init_tasks));
     println!("Планировщик запущен");
 
     loop {
         let current_time = Utc::now();
+        let mut tasks_guard = tasks.lock().await;
+        let mut list_changed = false;
 
-        for task in &mut tasks {
-            if let Some(run_time) = task.next_run {
+        let mut i = 0;
+        while i < tasks_guard.len() {
+            let mut run_task = false;
+            let mut sc_time = Utc::now();
+
+            if let Some(run_time) = tasks_guard[i].next_run {
                 if current_time >= run_time {
-                    if task.is_running {
-                        println!("Задача {} еще выполняется. скип", task.task_id);
-                        task.next_runj = Some(Utc::now() + chrono::TimeDelta::try_seconds(1).unwrap_or_default());
-                        continue;
-                    }
-
-                    task.is_running = true;
-                    
-                    let mut cmd = tokio::process::Command::new(&task.command);
-                    cmd.args(&task.args);
-                    cmd.stdout(Stdio::inherit());
-                    cmd.stderr(Stdio::inherit());
-                    match cmd.spawn() {
-                        Ok(child) => {
-                            println!("Процесс для задачи {} запущен", task.task_id)
-                            /*let tx_clone = tx.clone();
-                            let id = task.task_id;
-
-                            tokio::spawn(async move {
-                                let _ = child.wait().await;
-                                let _ = tx_clone.send(id).await;
-                            })*/
-                        }
-                        Err(e) => {
-                            eprintln!("Не удалось запустить задачу '{}': {}", task.command, e); 
-                            task.is_running = false;
-                        }
-                    }
-
-                    if let Trigger::Interval(secs) = task.trigger {
-                        task.next_run = Some(Utc::now() + chrono::TimeDelta::try_seconds(secs as i64).unwrap_or_default());
-                    }
+                    run_task = true;
+                    sc_time = run_time;
                 }
             }
+            if run_task {
+                let task = tasks_guard[i].clone();
+
+                let mut should_remove = false; 
+                match task.trigger {
+                    Trigger::Once => {
+                        should_remove = true;
+                    },
+                    Trigger::Interval(secs) => {
+                        tasks_guard[i].next_run = Some(sc_time + chrono::TimeDelta::try_seconds(secs as i64).unwrap_or_default());
+                        list_changed = true;
+                    }
+                    _ => {}
+                };
+
+            tokio::spawn(async move {
+                let mut cmd = tokio::process::Command::new(&task.command);
+                cmd.args(&task.args);
+
+                println!("задача {} запущена в фоне", task.task_id);
+
+                match cmd.spawn() {
+                    Ok(mut child) => {
+                        match child.wait().await {
+                            Ok(status) => println!("задача {} завершилась: {}", task.task_id, status),
+                            Err(e) => eprintln!("ошибка завершения задачи {}: {}", task.task_id, e),
+                        }
+                    }
+                    Err(e) => eprintln!("не удалось запустит задачу {}: {}", task.task_id, e)
+                }
+            });
+            if should_remove {
+                tasks_guard.remove(i);
+                list_changed = true;
+                continue;
+            }
+            }
+            i += 1;
         }
+        if list_changed {
+            save_task(path, &tasks_guard).await?;
+        }
+        drop(tasks_guard);
         tokio::time::sleep(Duration::from_secs(1)).await;
     }
-    Ok(())
 }
